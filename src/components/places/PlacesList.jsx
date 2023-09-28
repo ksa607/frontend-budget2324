bbc67432dce1ee4d46df8dc4c27b4bec5b090e80
Thomas Aelbrecht <thomasaelbrecht@live.com>
2023-09-28 14:45:57 +0200

    Attach PlacesList to API

--- src/components/places/PlacesList.jsx
+++ src/components/places/PlacesList.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react';
-import { PLACE_DATA } from '../../api/mock_data';
+import useSWR from 'swr';
+import useSWRMutation from 'swr/mutation';
+import { getAll, save, deleteById } from '../../api';
 import Place from './Place';
+import AsyncData from '../AsyncData';
+import { useCallback } from 'react';
 
 const PlacesList = () => {
-  const [places, setPlaces] = useState(PLACE_DATA);
+  const {
+    data: places = [],
+    error,
+    isLoading,
+  } = useSWR('places', getAll);
 
-  const handleRatePlace = (id, rating) => {
-    const newPlaces = places.map((p) => (p.id === id ? { ...p, rating } : p));
-    setPlaces(newPlaces);
-  };
+  const {
+    trigger: savePlace,
+    error: saveError,
+  } = useSWRMutation('places', save);
 
-  const handleDeletePlace = (id) => {
-    setPlaces((places) => places.filter((p) => p.id !== id));
-  };
+  const {
+    trigger: deletePlace,
+    error: deleteError,
+  } = useSWRMutation('places', deleteById);
+
+  const handleRatePlace = useCallback(async (id, rating) => {
+    const place = places.find((p) => p.id === id);
+    await savePlace({ ...place, rating });
+  }, [savePlace, places]);
+
+  const handleDeletePlace = useCallback(async (id) => {
+    await deletePlace(id);
+  }, [deletePlace]);
 
   return (
     <div className='grid mt-3'>
       <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-3'>
-        {places
-          .sort((a, b) =>
-            a.name.toUpperCase().localeCompare(b.name.toUpperCase())
-          )
-          .map((p) => (
-            <div className='col' key={p.id}>
-              <Place
-                {...p}
-                onRate={handleRatePlace}
-                onDelete={handleDeletePlace}
-              />
-            </div>
-          ))}
+        <AsyncData error={error || saveError || deleteError} loading={isLoading}>
+          {places
+            .sort((a, b) =>
+              a.name.toUpperCase().localeCompare(b.name.toUpperCase())
+            )
+            .map((p) => (
+              <div className='col' key={p.id}>
+                <Place
+                  {...p}
+                  onRate={handleRatePlace}
+                  onDelete={handleDeletePlace}
+                />
+              </div>
+            ))}
+        </AsyncData>
       </div>
     </div>
   );
