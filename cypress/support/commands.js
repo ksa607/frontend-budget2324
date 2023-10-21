b53976f96e8af51637dcd99cfae4454a95286f2f
Thomas Aelbrecht <thomasaelbrecht@live.com>
2023-10-21 17:03:24 +0200

    Wait for login in tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -29,10 +29,12 @@ Cypress.Commands.add('login', (email, password) => {
     displayName: 'login',
   });
 
+  cy.intercept('/api/users/login').as('login');
   cy.visit('http://localhost:5173/login');
   cy.get('[data-cy=email_input]').clear().type(email);
   cy.get('[data-cy=password_input]').clear().type(password);
   cy.get('[data-cy=submit_btn]').click();
+  cy.wait('@login');
 });
 
 Cypress.Commands.add('logout', () => {
