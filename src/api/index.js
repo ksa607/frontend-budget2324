fec9785e1e680353b16fd3b6a1a3e0226f0e626d
Thomas Aelbrecht <thomasaelbrecht@live.com>
2023-10-21 14:31:17 +0200

    Add POST api fetcher

--- src/api/index.js
+++ src/api/index.js
@@ -14,6 +14,14 @@ export const deleteById = async (url, { arg: id }) => {
   await axios.delete(`${baseUrl}/${url}/${id}`);
 };
 
+export const post = async (url, { arg }) => {
+  const {
+    data,
+  } = await axios.post(url, arg);
+
+  return data;
+};
+
 export const save = async (url, { arg: body }) => {
   const { id, ...values } = body;
   await axios({
